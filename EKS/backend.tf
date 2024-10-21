9aed0a4854019c4b0a6b0ce32e9a2e5457c70852
Ramesh-NB <ramurame1999@gmail.com>
2024-10-21 19:32:34 +0530

    Update backend.tf

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "acecloud-mega-project-s3bucket"  # Replace with your actual S3 bucket name
+    bucket = "amazons3_bucket_project"  # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"  
-    region = "ap-south-1" 
+    region = "ap-east-1" 
   }
 }
