be31f31916e4b58b75fcef1768f448b77152a30d
Ramesh-NB <ramurame1999@gmail.com>
2024-10-21 19:59:50 +0530

    Update backend.tf

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "amazons3_bucket_project"  # Replace with your actual S3 bucket name
+    bucket = "amazons3_ramesh_bucket"  # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"  
     region = "us-east-1" 
   }
